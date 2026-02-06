c42740cf695a39fcac1a9d7ef92076d410b5702d
VazhaK14 <khayrivazhasocial@gmail.com>
2026-02-06 23:20:35 +0700

    feat: cta

---
+++ src/components/CTA.tsx
@@ -0,0 +1,25 @@
+import { ContactIcon } from "lucide-react";
+import { Button } from "./ui/button";
+
+export default function CTA() {
+  return (
+    <div className="px-8 py-10 md:px-20 md:py-12 flex flex-col md:flex-row rounded-xl items-center shadow-lg gap-8 md:gap-0 bg-white">
+      <div className="flex flex-col gap-4 md:gap-10 items-center md:items-start text-center md:text-left">
+        <h2 className="text-2xl font-bold md:text-4xl">
+          Kamu punya <br className="md:hidden" /> pertanyaan?
+        </h2>
+        <p className="text-sm md:text-base text-gray-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+          eveniet perspiciatis rem error quam, excepturi tempora eligendi
+          obcaecati, omnis, quia laudantium est? Nobis itaque porro quas
+          excepturi. Magni, magnam suscipit.
+        </p>
+        <Button className="flex w-fit max-md:text-xs flex-row gap-3">
+          <ContactIcon className="w-4 h-4" />
+          Contact Us
+        </Button>
+      </div>
+      <img src="/cta.svg" alt="CTA icon" className="md:hidden" />
+    </div>
+  );
+}
--- src/modules/LandingPageModule/index.tsx
+++ src/modules/LandingPageModule/index.tsx
@@ -1,19 +1,22 @@
 import React from "react";
-import HeroSection from "./components/HeroSection"; 
+import HeroSection from "./components/HeroSection";
 import OurProjectSection from "./components/OurProjectSection";
 import OurBenefitSection from "./components/OurBenefitSection";
 import OurPartnerSection from "./components/OurPartnersSection";
 import OurClientSection from "./components/OurClientSection";
+import CTA from "@/components/CTA";
 
 const LandingPageModule = () => {
   return (
     <main className="flex flex-col w-full">
-
       <HeroSection />
       <OurProjectSection />
       <OurBenefitSection />
       <OurPartnerSection />
       <OurClientSection />
+      <div className=" px-10 md:px-20 my-20">
+        <CTA />
+      </div>
     </main>
   );
 };
